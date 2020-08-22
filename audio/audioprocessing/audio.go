a2b20bb8ec457f7ab4ab0237e6562f1dbbd42da9
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-08-22 12:39:31 +0100

    adding Player

--- audio/audioprocessing/audio.go
+++ audio/audioprocessing/audio.go
@@ -1,14 +1,38 @@
 package audioprocessing
 
 import (
-	"github.com/xfrr/goffmpeg/ffmpeg"
 	"github.com/xfrr/goffmpeg/transcoder"
-
-	"../config"
-	"../utils"
+	"log"
 )
 
 type FFmpegPlayer struct {
-	trans *transcoder.Transcoder
-	tracks
+	PlayerNo int
+	Trans    *transcoder.Transcoder
+	Tracks   TrackManager
+}
+
+func (fp *FFmpegPlayer) Queue(t Track) int {
+	index := fp.Tracks.AddTrack(t)
+	log.Printf("[ Player %v ] Queued track at position %v", fp.PlayerNo, index)
+	return index
+}
+
+func (fp *FFmpegPlayer) RemoveTrackByIndex(n int) bool {
+	success, track := fp.Tracks.RemoveTrackByIndex(n)
+	if success {
+		log.Printf("[ Player %v ] Removed track %v", fp.PlayerNo, track.Id)
+	} else {
+		log.Printf("[ Player %v ] Failed to removed track as index %v", fp.PlayerNo, n)
+	}
+	return success
+}
+
+func (fp *FFmpegPlayer) RemoveTrackById(id string) bool {
+	success, track := fp.Tracks.RemoveTrackById(id)
+	if success {
+		log.Printf("[ Player %v ] Removed track %v", fp.PlayerNo, track.Id)
+	} else {
+		log.Printf("[ Player %v ] Failed to removed track with id %v", fp.PlayerNo, id)
+	}
+	return success
 }
